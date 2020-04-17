6491fcda73385c5e159e35ebf89e9fab508faa93
zortraxfiap <63025656+zortraxfiap@users.noreply.github.com>
2020-04-16 22:40:20 -0300

    Update role.tf

--- role.tf
+++ role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecr_readOnly_role" {
-  name = "ecr_readOnly_role"
+  name = "${var.filename}"
 
   assume_role_policy = <<EOF
 {
@@ -18,7 +18,7 @@ resource "aws_iam_role" "ecr_readOnly_role" {
 EOF
 
   tags = {
-      Name = "ecr_readOnly_role"
+      Name = "${var.filename}"
   }
 }
 
@@ -28,7 +28,7 @@ resource "aws_iam_instance_profile" "ecr_readOnly_profile" {
 }
 
 resource "aws_iam_role_policy" "ecr_readOnly_policy" {
-  name = "ecr_readOnly_policy"
+  name = "${var.filename}"
   role = "${aws_iam_role.ecr_readOnly_role.id}"
 
   policy = <<EOF
